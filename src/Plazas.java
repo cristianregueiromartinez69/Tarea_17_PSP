5ef953e6c6fb5ade1e953da328a2049da36cedfb
Cristian <cregueiromartinez@danielcastelao.org>
2024-11-14 09:09:17 +0100

    creando clase plazas

---
+++ src/Plazas.java
@@ -0,0 +1,24 @@
+public class Plazas {
+
+    private int numPlazasOcupadas = 0;
+    private int minPlazas  = 0;
+    private int maxPlazas = 20;
+
+    public synchronized void aumentarPlazas() {
+        numPlazasOcupadas++;
+    }
+
+    public synchronized void disminuirNumPlazas() {
+        numPlazasOcupadas--;
+    }
+
+    public synchronized int getNumPlazasOcupadas() {
+        return numPlazasOcupadas;
+    }
+    public synchronized int getMinPlazas() {
+        return minPlazas;
+    }
+    public synchronized int getMaxPlazas() {
+        return maxPlazas;
+    }
+}
