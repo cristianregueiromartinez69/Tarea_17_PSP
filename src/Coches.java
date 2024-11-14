dfa1d2f4f32fca11f58145fcc36c3510f9fb7e8c
cristian <cregueiromartinez@danielcastelao.org>
2024-11-14 19:28:28 +0100

    documentacion hecha

--- src/Coches.java
+++ src/Coches.java
@@ -1,5 +1,14 @@
+/**
+ * Clase para establecer el numero de coches que habrá en el aparcamiento
+ * @author cristian
+ * @version 1.0
+ */
 public class Coches {
 
+    /**
+     * metodo que devuelve el numero de coches
+     * @return el numero de coches
+     */
     public int getNumCoches() {
         return 10;
     }
--- src/Hilo.java
+++ src/Hilo.java
@@ -1,18 +1,34 @@
 import java.util.Random;
 
+/**
+ * Clase hilo que implementa la interfaz Runnable
+ * @author cristian
+ * @version 1.0
+ */
 public class Hilo implements Runnable {
+    //variables finales de clase
     private final Parking parking;
     private final int idCoche;
 
+    /**
+     * Constructor de la clase
+     * @param parking el parking de coches
+     * @param idCoche el id del coche
+     */
     public Hilo(Parking parking, int idCoche) {
         this.parking = parking;
         this.idCoche = idCoche;
     }
 
+    /**
+     * metodo para iniciar el hilo
+     */
     @Override
     public void run() {
+        //bucle infinito
         while (true) {
             try {
+                //llamamos a los metodos y dormimos el hilo
                 parking.logicaAparcar(idCoche);
                 Thread.sleep(new Random().nextInt(1000) + 3000);
                 parking.logicaSalir(idCoche);
